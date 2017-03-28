c7c1c02c6453ffdbc8289a1a62b54e686824b346
Tom Scott <tomscott85@gmail.com>
2017-03-28 21:42:28 +1100

    add comment list test/component

---
+++ src/components/comment_list.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const CommentList = (props) => {
+  const list = props.comments.map(comment => <li key={comment}>{comment}</li>)
+  return (
+    <div>
+      <ul className="comment-list">{list}</ul>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return { comments: state.comments }
+}
+
+export default connect(mapStateToProps)(CommentList)
