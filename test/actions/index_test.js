58a0a126b47ec7336807e17b63b9e8a02cd5dae4
Tom Scott <tomscott85@gmail.com>
2017-03-29 21:45:37 +1100

    update spelling mistake

--- test/actions/index_test.js
+++ test/actions/index_test.js
@@ -1,8 +1,8 @@
-import { expect } from '../test-helper'
+import { expect } from '../test_helper'
 import { SAVE_COMMENT } from '../../src/actions/types'
 import { saveComment } from '../../src/actions'
 
-descirbe('actions', () => {
+describe('actions', () => {
 
   describe('saveComment', () => {
     it('has the correct type', () => {
